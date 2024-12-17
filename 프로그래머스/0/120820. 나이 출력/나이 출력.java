6e949a7b20547ce837637345b39c41b655173744
jieun1020 <qkrwldms292@gmail.com>
2024-12-17 19:32:26 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int currentYear = 2022;
+    
+        answer = (currentYear - age) + 1;
+        return answer;
+    }
+}
