f3ca8dad3280fe39d5d44b5c4a33673427518345
jieun1020 <qkrwldms292@gmail.com>
2024-12-17 19:29:50 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    var answer = '';
+    for(let i = 0; i <s.length; i++){
+        if(s.length % 2 == 0){
+            return s[s.length/2 -1] + s[s.length/2]
+        }else{
+            return s[Math.floor(s.length/2)]
+        }
+    }
+}
